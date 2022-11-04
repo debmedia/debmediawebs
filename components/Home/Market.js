d7372c9c93ae0fdf9cd619f5a43104fc2dd6cd73
Agustín Queija <agustin.queija@gmail.com>
2022-11-04 19:35:18 -0300

    Traduccion Market del Home

--- components/Home/Market.js
+++ components/Home/Market.js
@@ -6,9 +6,10 @@ import pepole from '../../asset/imgs/home/pepole.svg'
 import connect from '../../asset/imgs/home/connect.svg'
 import stats from '../../asset/imgs/home/stats.svg'
 import arrow from '../../asset/imgs/home/arrow.svg'
-
+import { useTranslation, Trans } from 'next-i18next';
 
 export default function Market() {
+    const { t } = useTranslation(['home', 'common']);
     return (
         <>
             <Container fluid className="market-section">
@@ -16,7 +17,7 @@ export default function Market() {
                     <Row>
                         <Col className="market-section_maintitle">
                             <h3 className="text-center">
-                                El mercado cambió y es necesario adaptarse.
+                                {t("MARKET.TITLE")}
                             </h3>
                         </Col>
                     </Row>
@@ -26,38 +27,21 @@ export default function Market() {
                                 <Image src={pepole.src} width={90}
                                     height={135} className={'img-fluid'} />
                             </div>
-                            <h4 className="market-section_title">
-                                Clientes insatisfechos
-                            </h4>
-                            <p className="market-section_parraf">
-                                por una atención ineficiente.
-                            </p>
+                            <Trans t={t} i18nKey={"MARKET.CAPTION_1"}/>
                         </Col>
                         <Col sm={4} xs={4} className="market-section_div">
                             <div className="imagenXS">
                                 <Image src={stats.src} width={100}
                                     height={135} className={'img-fluid'} />
                             </div>
-
-                            <h4 className="market-section_title">
-                                Procesos
-                            </h4>
-                            <p className="market-section_parraf">
-                                de atención costosos
-
-                            </p>
+                            <Trans t={t} i18nKey={"MARKET.CAPTION_2"}/>
                         </Col>
                         <Col sm={4} xs={4} className="market-section_div">
                             <div className="imagenXS">
                                 <Image src={connect.src} width={90}
                                     height={135} className={'img-fluid'} />
                             </div>
-                            <h4 className="market-section_title">
-                                Desconexión entre canales
-                            </h4>
-                            <p className="market-section_parraf">
-                                digitales y presenciales.
-                            </p>
+                            <Trans t={t} i18nKey={"MARKET.CAPTION_3"}/>
                         </Col>
                     </Row>
                 </Container>
