e71874549b8d09761214f3b59084197f0b740e8f
Agustín Queija <agustin.queija@gmail.com>
2022-11-04 22:01:00 -0300

    Agregar config que me parece que no funciona

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,5 +3,11 @@ module.exports = {
     defaultLocale: 'es',
     locales: ['es', 'pt'],
     reloadOnPrerender: true,//sacar en producción
+    options: {
+      react: {
+        transSupportBasicHtmlNodes: true,
+        transKeepBasicHtmlNodesFor: ['br', 'strong', 'i', 'p', 'b']
+      }
+    }
   },
 };
