5ebafa6d14edc8e7424da611c8f41824f96f4f88
Agustín Queija <agustin.queija@gmail.com>
2022-11-23 14:42:52 -0300

    Traduccion de Politicas

--- pages/politica.js
+++ pages/politica.js
@@ -1,10 +1,26 @@
-import React, { useState, useEffect } from "react";
-import Contact from "../components/Contact"
+import React from "react";
+import ReactMarkdown from 'react-markdown';
+import politica_es from '../md/politica-es.md';
+import { useTranslation} from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useRouter } from "next/router";
 
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['politica','components','common'])),
+    },
+  };
+}
 
+const politcas_lang = {
+    es: politica_es,
+}
 
-export default function debQ() {
-
+export default function Policy() {
+    const { t } = useTranslation(['politica', 'common']);
+    const { locale } = useRouter();
+    const politicas = politcas_lang[locale] || politcas_lang['es'];
 
   return (
     <>
@@ -14,27 +30,16 @@ export default function debQ() {
             <div className="container mt-4">
                 <div className="row">
                     <div className="col-12">
-                        <h1 className="titSection text-center">Política de Calidad y Seguridad</h1>
+                        <h1 className="titSection text-center">
+                            { t("TITLE") }
+                        </h1>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-12 mt-3">
-                        <p>Debmedia es una organización dedicada a diseñar y desarrollar soluciones tecnológicas para mejorar la experiencia de clientes en sucursales; enfocada en optimizar la relación entre las empresas y sus clientes.</p>
-                        <p>
-                        Determina su política integrada de gestión comprometiéndose a:
-                        </p>
-                        <ul>
-                            <li>Garantizar la continuidad operativa enfocados en los productos y servicios que la empresa comercializa en la industria del Customer Experience Management (CEM), a través de una eficaz administración y gestión de las plataformas operativas que lo soportan, proporcionando soluciones integrales de tecnología de la información y de la comunicación, promoviendo la mejora continua.</li>
-                            <li>Satisfacer las expectativas de sus partes interesadas y el cumplimiento con los requisitos establecidos por las leyes aplicables y otros que afectan al Sistema de Gestión Integrado (SGI), logrando un nivel de servicio de primer nivel en el ámbito regional.</li>
-                            <li>Establecer objetivos de calidad y de seguridad de la información basados en el marco de trabajo para tomar en consideración sus resultados.</li>
-                            <li>Llevar a cabo el seguimiento para desarrollar las acciones necesarias que garanticen el cumplimiento de los requisitos aplicables al SGI.</li>
-                            <li>Asegurar la confidencialidad y la integridad de la información sensible que se maneja en todos los servicios ofrecidos, estableciendo criterios para la identificación y evaluación de riesgos.</li>
-                            <li>Apoyar la formación y el desarrollo de los empleados como así también la concientización en materia de las actividades relacionadas a la Seguridad de la Información.</li>
-                            <li>Proporcionar y fomentar un ambiente de trabajo, donde todo el personal de la Organización sea responsable de reportar los eventos de seguridad que detecte, que reconozca, valore y vele por la integridad de la información y por todas las actividades que se desarrollan a tal fin.</li>
-                            <li>Ofrecer servicios a nuestros clientes contando con personal altamente capacitado en los procesos de diseño, adecuaciones y desarrollo de productos y servicios tecnológicos, así como incorporar a la seguridad de la información </li>
-                            
-                        </ul>
-                        <p><strong>Esta política es revisada para su adecuación de forma anual por la Dirección de la organización. La misma se encuentra disponible a disposición de las partes interesadas.</strong></p>
+                        <ReactMarkdown>
+                            {politicas}
+                        </ReactMarkdown>
                     </div>
                 </div>
             </div>
