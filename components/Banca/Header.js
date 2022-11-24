522aff63a5f583ba811429103c171c86385d25aa
Agustín Queija <agustin.queija@gmail.com>
2022-11-24 09:06:10 -0300

    Sacar cosas que no se usan

--- components/Banca/Header.js
+++ components/Banca/Header.js
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Row, Container, Col } from "react-bootstrap";
-import Image from 'next/image'
-import mypic from '../../asset/imgs/home/videoShape.svg'
-import shape1 from '../../asset/imgs/home/headershape1.svg'
 import shape2 from '../../asset/imgs/home/headershape2.svg'
-import shape3 from '../../asset/imgs/home/headershape3.svg'
-import { useMediaQuery } from 'react-responsive'
 import Link from 'next/link'
 
 export default function Header() {
-    const isMobile = useMediaQuery({ query: '(max-width:899px)' })
 
     return (
         <>
