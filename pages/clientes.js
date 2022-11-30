974192370bfac0979d2ddc21a4a77a2d7d933eb4
Agustín Queija <agustin.queija@gmail.com>
2022-11-30 14:47:53 -0300

    TRADU CLIENTS > SOLUTIONS

--- pages/clientes.js
+++ pages/clientes.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Clientes from "../components/Clientes/Solutions"
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['clientes','components','common'])),
+    },
+  };
+}
 
 
 export default function Clients() {
