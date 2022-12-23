1b1e492a9ac64883d75f2e298e5d9fede85f0043
Agustín Queija <agustin.queija@gmail.com>
2022-12-23 17:13:39 -0300

    tradu encuesas > modern

--- components/Encuestas/Modern.js
+++ components/Encuestas/Modern.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Row, Container, Col } from "react-bootstrap";
 import Image from 'next/image'
 import modern1 from '../../asset/imgs/encuestas/modern1.png'
 import modern2 from '../../asset/imgs/encuestas/modern2.png'
 import modern3 from '../../asset/imgs/encuestas/modern3.png'
-
-
+import { useTranslation, Trans } from 'next-i18next';
 
 export default function Modern() {
+    const { t } = useTranslation(['encuestas', 'common']);
+
     return (
         <>
             <Container fluid className="modern-section mb-5">
                 <Container>
                     <Row>
                         <Col className="modern-section_maintitle">
-                            <h3 className="text-center">
-                                <b>Eleva la tasa de respuesta </b><br className="d-none d-sm-block" />
-                                con encuestas de satisfacción hasta 10 veces más.
+                            <h3 className="text-center no-br-sm">
+                                <Trans t={t} i18nKey="MODERN.TITLE"/>
                             </h3>
                         </Col>
                     </Row>
@@ -26,32 +26,26 @@ export default function Modern() {
                                 <Col sm={6} xs={12} className="modern-section_div-img d-flex justify-content-center">
                                     <Image src={modern1.src} width={350}
                                         height={350} />
-
                                 </Col>
                                 <Col sm={6} xs={12} className="modern-section_div-text">
                                     <div>
                                         <h4 className="modern-section_title">
-                                            Personaliza diferentes <br />
-                                            encuestas
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_1.TITLE"/>
                                         </h4>
                                         <p className="modern-section_parraf">
-                                            para cada tipo
-                                            de trámite.
-
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_1.SUBTITLE"/>
                                         </p>
                                     </div>
-
                                 </Col>
                             </Row>
                             <Row className="d-sm-flex reverseXs">
                                 <Col sm={5} xs={12} className="modern-section_div-text modern-section_div-text-center">
                                     <div>
                                         <h4 className="modern-section_title">
-                                            Envía encuestas
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_2.TITLE"/>
                                         </h4>
                                         <p className="modern-section_parraf">
-                                            en diferentes momentos
-                                            del viaje del cliente.
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_2.SUBTITLE"/>
                                         </p>
                                     </div>
 
@@ -60,7 +54,6 @@ export default function Modern() {
                                     <Image src={modern3.src} width={350}
                                         height={350} />
                                 </Col>
-
                             </Row>
                             <Row>
                                 <Col sm={6} xs={12} className="modern-section_div-img d-flex justify-content-center">
@@ -70,18 +63,15 @@ export default function Modern() {
                                 <Col sm={6} xs={12} className="modern-section_div-text">
                                     <div>
                                         <h4 className="modern-section_title">
-                                            Compara los resultados
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_3.TITLE"/>
                                         </h4>
                                         <p className="modern-section_parraf">
-                                            por periodos de tiempo, usuario, atención y más.
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_3.SUBTITLE"/>
                                         </p>
                                     </div>
-
                                 </Col>
                             </Row>
                         </Col>
-
-
                     </Row>
                 </Container>
             </Container>
