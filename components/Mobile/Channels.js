e5d84200a9b336458a1bafbb32a1fb17c805971f
Agustín Queija <agustin.queija@gmail.com>
2022-12-26 13:13:23 -0300

    Tradu fila-virtual > channels

--- components/Mobile/Channels.js
+++ components/Mobile/Channels.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Row, Container, Col } from "react-bootstrap";
 import Image from 'next/image'
 import mypic from '../../asset/imgs/mobile/mainbanner.png'
 import shape2 from '../../asset/imgs/mobile/shapebanner.svg'
-
+import { useTranslation, Trans } from 'next-i18next';
 
 export default function Market() {
+    const { t } = useTranslation(['fila-virtual', 'common']);
+
     return (
         <>
             <Container fluid className="channel-mobile-section relative">
@@ -16,8 +18,7 @@ export default function Market() {
                     <Row>
                         <Col sm="4" className="channel-mobile-section_maintitle">
                             <h3>
-                                <b>Permite a tus clientes</b><br /> tomar su turno desde <br />
-                                <b>diferentes canales.</b>
+                                <Trans t={t} i18nKey="CHANNELS.TITLE"/>
                             </h3>
                         </Col>
                         <Col sm="8">
