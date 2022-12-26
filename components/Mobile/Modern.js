80e3f352e6659f3b7631d00b87f1de8c56b5c0c1
Agustín Queija <agustin.queija@gmail.com>
2022-12-26 15:51:25 -0300

    tradu fila-virtual > modern

--- components/Mobile/Modern.js
+++ components/Mobile/Modern.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Row, Container, Col } from "react-bootstrap";
 import Image from 'next/image'
 import modern1 from '../../asset/imgs/mobile/modern1.png'
 import modern2 from '../../asset/imgs/mobile/modern2.png'
 import modern3 from '../../asset/imgs/mobile/modern3.png'
 import modern4 from '../../asset/imgs/mobile/modern4.png'
-
-
+import { useTranslation, Trans } from 'next-i18next';
 
 export default function Modern() {
+    const { t } = useTranslation(['fila-virtual', 'common']);
+
     return (
         <>
             <Container fluid className="modern-section">
                 <Container>
                     <Row>
                         <Col className="modern-section_maintitle">
-                            <h3 className="text-center">
-                                <b>Agiliza los tiempos de espera </b><br className="d-none d-sm-block"/>
-                                dentro de tus sucursales
-
+                            <h3 className="text-center no-br-sm">
+                                <Trans t={t} i18nKey="MODERN.TITLE"/>
                             </h3>
                         </Col>
                     </Row>
@@ -28,40 +27,33 @@ export default function Modern() {
                                 <Col sm={6} xs={12} className="modern-section_div-img d-flex justify-content-center">
                                     <Image src={modern1.src} width={330}
                                         height={420} />
-
                                 </Col>
                                 <Col sm={6} xs={12} className="modern-section_div-text">
                                     <div>
                                         <h4 className="modern-section_title">
-                                            Integrable con chatbot
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_1.TITLE"/>
                                         </h4>
                                         <p className="modern-section_parraf">
-                                            El cliente puede ingresar y<br /> seguir la fila desde WhatsApp.
-
-
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_1.SUBTITLE"/>
                                         </p>
                                     </div>
-
                                 </Col>
                             </Row>
                             <Row className="d-sm-flex reverseXs">
                                 <Col sm={5} xs={12} className="modern-section_div-text modern-section_div-text-center">
                                     <div>
                                         <h4 className="modern-section_title">
-                                            Integrable por API o Iframe
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_2.TITLE"/>
                                         </h4>
                                         <p className="modern-section_parraf">
-                                            A una aplicación o web pre existente.
-
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_2.SUBTITLE"/>
                                         </p>
                                     </div>
-
                                 </Col>
                                 <Col sm={7} xs={12} className="modern-section_div-img d-flex justify-content-center">
                                     <Image src={modern2.src} width={300}
                                         height={300} />
                                 </Col>
-
                             </Row>
                             <Row>
                                 <Col sm={6} xs={12} className="modern-section_div-img d-flex justify-content-center">
@@ -71,39 +63,31 @@ export default function Modern() {
                                 <Col sm={6} xs={12} className="modern-section_div-text">
                                     <div>
                                         <h4 className="modern-section_title">
-                                            Configuración de trámites
-
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_3.TITLE"/>
                                         </h4>
                                         <p className="modern-section_parraf">
-                                            Para seleccionar a cuáles los clientes se pueden encolar a través de fila virtual.
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_3.SUBTITLE"/>
                                         </p>
                                     </div>
-
                                 </Col>
                             </Row>
                             <Row className="d-sm-flex reverseXs">
                                 <Col sm={5} xs={12} className="modern-section_div-text modern-section_div-text-center">
                                     <div>
                                         <h4 className="modern-section_title">
-                                            Look and feel personalizado
-
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_4.TITLE"/>
                                         </h4>
                                         <p className="modern-section_parraf">
-                                            Para mantener la identidad de la organización
-
+                                            <Trans t={t} i18nKey="MODERN.CAPTION_4.SUBTITLE"/>
                                         </p>
                                     </div>
-
                                 </Col>
                                 <Col sm={7} xs={12} className="modern-section_div-img d-flex justify-content-center">
                                     <Image src={modern4.src} width={400}
                                         height={450} />
                                 </Col>
-
                             </Row>
                         </Col>
-
-
                     </Row>
                 </Container>
             </Container>
