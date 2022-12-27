4e1408ea128c2b61f7f7cd60e32b225b2b65aa51
Agustín Queija <agustin.queija@gmail.com>
2022-12-27 13:44:50 -0300

    tradu > nosotros > market

--- components/Nosotros/Market.js
+++ components/Nosotros/Market.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Row, Container, Col } from "react-bootstrap";
-import Image from 'next/image'
 import celda from '../../asset/imgs/nosotros/celda.svg'
-import celdaxs from '../../asset/imgs/nosotros/celda-xs.svg'
-import video from '../../asset/imgs/nosotros/celda-xs.svg'
-
+import { useTranslation, Trans } from 'next-i18next';
 
 export default function Market() {
+    const { t } = useTranslation(['nosotros', 'common']);
+
     return (
         <>
             <Container fluid className="market-nosotros-section">
                 <Container>
                     <Row>
                         <Col className="col-12 market-nosotros-section_blue market-nosotros-section_maintitle">
-                            <h3 className="text-center">
-                                ¿Qué queremos hacer  <br className="d-none d-sm-block" />
-                                por <b>las empresas?</b>
-
+                            <h3 className="text-center no-br-sm">
+                                <Trans t={t} i18nKey="MARKET.TITLE"/>
                             </h3>
                             <p>
-                                No importa si hablamos de bancos, aseguradoras, hospitales o gobiernos.<b> Los clientes quieren elegir cuándo y a través de qué canal online y offline quieren interactuar.</b>
+                                <Trans t={t} i18nKey="MARKET.PARAGRAPH_1"/>
                             </p>
                             <p>
-                                En Debmedia ayudamos a distintas organizaciones a integrar sus canales de atención, optimizar sus recursos y ofrecer una experiencia consistente a sus clientes en cada punto de contacto a través de diferentes soluciones.
+                                <Trans t={t} i18nKey="MARKET.PARAGRAPH_2"/>
                             </p>
                         </Col>
                         <Col className="col-12 text-center mt-4">
