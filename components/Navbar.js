9208cd3fee690060beefbf8cf8281e633049c11a
Agustín Queija <agustin.queija@gmail.com>
2023-01-23 17:36:56 -0300

    Arreglar links de nuevo

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,7 +23,11 @@ export default function Home() {
 
     return (
         <>
-            <Navbar expanded={expanded} fixed="top" expand="lg" className={scroll ? "navScroll" : ""}>
+            <Navbar
+                expanded={expanded}
+                fixed="top"
+                expand="lg"
+                className={scroll ? "navScroll" : ""}>
                 <Container>
                     <Navbar.Brand href="#home">
                         <Link href="/">
@@ -38,98 +42,166 @@ export default function Home() {
                             </div>
                         </Link>
                     </Navbar.Brand>
-                    <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Navbar.Toggle
+                        onClick={() =>
+                            setExpanded(expanded ? false : "expanded")
+                        }
+                        aria-controls="basic-navbar-nav"
+                    />
+                    <Navbar.Collapse
+                        id="basic-navbar-nav"
+                        className="justify-content-end">
                         <Nav activeKey={router.pathname} className="ml-auto">
                             <Nav.Link href={`/${localePath}`}>
                                 {t("NAVBAR.HOME")}
                             </Nav.Link>
-                            <NavDropdown title={t("NAVBAR.SOLUTIONS.TITLE")} id="basic-nav-dropdown">
-                                <NavDropdown.Item  onClick={() => setExpanded(false)}>
-                                    <Link className="fulllink" href="/atencion-virtual">
-                                        {t("NAVBAR.SOLUTIONS.VIRTUAL_ATTENTION")}
+                            <NavDropdown
+                                title={t("NAVBAR.SOLUTIONS.TITLE")}
+                                id="basic-nav-dropdown">
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
+                                    <Link
+                                        className="fulllink"
+                                        href="/atencion-virtual">
+                                        {t(
+                                            "NAVBAR.SOLUTIONS.VIRTUAL_ATTENTION"
+                                        )}
                                     </Link>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => setExpanded(false)}>
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
                                     <Link href="/preatendedor">
                                         {t("NAVBAR.SOLUTIONS.WHATSAPP_CHATBOT")}
                                     </Link>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => setExpanded(false)}>
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
                                     <Link href="/fila-virtual">
                                         {t("NAVBAR.SOLUTIONS.VIRTUAL_QUEUE")}
                                     </Link>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => setExpanded(false)}>
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
                                     <Link href="/citasonline">
                                         {t("NAVBAR.SOLUTIONS.APPOINTMENTS")}
                                     </Link>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => setExpanded(false)}>
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
                                     <Link href="/debq">
                                         {t("NAVBAR.SOLUTIONS.QUEUE_MANAGEMENT")}
                                     </Link>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => setExpanded(false)}>
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
                                     <Link href="/debsign">
                                         {t("NAVBAR.SOLUTIONS.DIGITAL_SIGNAGE")}
                                     </Link>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => setExpanded(false)}>
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
                                     <Link href="/preatendedor">
                                         {t("NAVBAR.SOLUTIONS.PRE_SERVICE")}
                                     </Link>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => setExpanded(false)}>
+                                <NavDropdown.Item
+                                    onClick={() => setExpanded(false)}>
                                     <Link href="/encuestas">
                                         {t("NAVBAR.SOLUTIONS.SURVEYS")}
                                     </Link>
                                 </NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href={`${localePath}/clientes`}>{t("NAVBAR.CLIENTS")}</Nav.Link>
-                            <Nav.Link href={`${localePath}/nosotros`}>{t("NAVBAR.US")}</Nav.Link>
-                            <Nav.Link href={`${localePath}/partners`}>{t("NAVBAR.PARTNERS")}</Nav.Link>
-                            {router.locale === "es" && 
-                                <NavDropdown title={t("NAVBAR.RESOURCES.TITLE")} id="basic-nav-dropdown">
+                            <Link href={`/clientes`} passHref={true}>
+                                <Nav.Link>{t("NAVBAR.CLIENTS")}</Nav.Link>
+                            </Link>
+                            <Link href={`/nosotros`} passHref={true}>
+                                <Nav.Link>{t("NAVBAR.US")}</Nav.Link>
+                            </Link>
+                            <Link href={`/partners`} passHref={true}>
+                                <Nav.Link>{t("NAVBAR.PARTNERS")}</Nav.Link>
+                            </Link>
+                            {router.locale === "es" && (
+                                <NavDropdown
+                                    title={t("NAVBAR.RESOURCES.TITLE")}
+                                    id="basic-nav-dropdown">
                                     <NavDropdown.Item href="https://debmedia.com/blog/">
                                         <Link href="https://debmedia.com/blog/">
                                             {t("NAVBAR.RESOURCES.BLOG")}
                                         </Link>
                                     </NavDropdown.Item>
-                                    <NavDropdown.Item href="https://blog.debmedia.com/category/casos-de-exito/" onClick={() => setExpanded(false)}>
+                                    <NavDropdown.Item
+                                        href="https://blog.debmedia.com/category/casos-de-exito/"
+                                        onClick={() => setExpanded(false)}>
                                         <Link href="https://blog.debmedia.com/category/casos-de-exito/">
-                                            {t("NAVBAR.RESOURCES.SUCCESS_STORIES")}
+                                            {t(
+                                                "NAVBAR.RESOURCES.SUCCESS_STORIES"
+                                            )}
                                         </Link>
-
                                     </NavDropdown.Item>
-                                    <NavDropdown.Item href="https://blog.debmedia.com/category/casos-de-exito/" onClick={() => setExpanded(false)}>
+                                    <NavDropdown.Item
+                                        href="https://blog.debmedia.com/category/casos-de-exito/"
+                                        onClick={() => setExpanded(false)}>
                                         <Link href="https://blog.debmedia.com/category/ebooks/">
-                                            {t("NAVBAR.RESOURCES.DOWNLOADABLE_RESOURCES")}
+                                            {t(
+                                                "NAVBAR.RESOURCES.DOWNLOADABLE_RESOURCES"
+                                            )}
                                         </Link>
-
                                     </NavDropdown.Item>
                                 </NavDropdown>
-                            }
-                            <LanguageSelector className={"d-lg-none"} langs={[
-                            {key: "es", label: "LANGUAGE_SELECTOR.SPANISH"},
-                            {key: "en", label: "LANGUAGE_SELECTOR.ENGLISH"},
-                            {key: "pt", label: "LANGUAGE_SELECTOR.PORTUGUESE"},
-                            {key: "fr", label: "LANGUAGE_SELECTOR.FRENCH"},
-                           ]}/>
+                            )}
+                            <LanguageSelector
+                                className={"d-lg-none"}
+                                langs={[
+                                    {
+                                        key: "es",
+                                        label: "LANGUAGE_SELECTOR.SPANISH",
+                                    },
+                                    {
+                                        key: "en",
+                                        label: "LANGUAGE_SELECTOR.ENGLISH",
+                                    },
+                                    {
+                                        key: "pt",
+                                        label: "LANGUAGE_SELECTOR.PORTUGUESE",
+                                    },
+                                    {
+                                        key: "fr",
+                                        label: "LANGUAGE_SELECTOR.FRENCH",
+                                    },
+                                ]}
+                            />
                             <Link href="/contacto">
-                                <Nav.Link href="#link" className="demo text-nowrap">{t("common:REQUEST_A_DEMO")}</Nav.Link>
+                                <Nav.Link
+                                    href="#link"
+                                    className="demo text-nowrap">
+                                    {t("common:REQUEST_A_DEMO")}
+                                </Nav.Link>
                             </Link>
-                           <LanguageSelector className={"d-none d-lg-block"} langs={[
-                            {key: "es", label: "LANGUAGE_SELECTOR.SPANISH"},
-                            {key: "en", label: "LANGUAGE_SELECTOR.ENGLISH"},
-                            {key: "pt", label: "LANGUAGE_SELECTOR.PORTUGUESE"},
-                            {key: "fr", label: "LANGUAGE_SELECTOR.FRENCH"},
-                           ]}/>
+                            <LanguageSelector
+                                className={"d-none d-lg-block"}
+                                langs={[
+                                    {
+                                        key: "es",
+                                        label: "LANGUAGE_SELECTOR.SPANISH",
+                                    },
+                                    {
+                                        key: "en",
+                                        label: "LANGUAGE_SELECTOR.ENGLISH",
+                                    },
+                                    {
+                                        key: "pt",
+                                        label: "LANGUAGE_SELECTOR.PORTUGUESE",
+                                    },
+                                    {
+                                        key: "fr",
+                                        label: "LANGUAGE_SELECTOR.FRENCH",
+                                    },
+                                ]}
+                            />
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
         </>
     );
 }
