d9a28141bd64d4a874cad2705ba5a5705280214d
Agustín Queija <agustin.queija@gmail.com>
2023-02-17 12:06:18 -0300

    tradu img encuestas flow

--- components/Encuestas/Flow.js
+++ components/Encuestas/Flow.js
@@ -1,14 +1,27 @@
+import { Trans, useTranslation } from 'next-i18next';
+import { useRouter } from "next/dist/client/router";
+import Link from 'next/link';
 import React from "react";
-import { Row, Container, Col } from "react-bootstrap";
-import how1 from '../../asset/imgs/encuestas/how1.png'
-import how2 from '../../asset/imgs/encuestas/how2.png'
-import how3 from '../../asset/imgs/encuestas/how3.png'
-import Link from 'next/link'
-import { useTranslation, Trans } from 'next-i18next';
+import { Col, Container, Row } from "react-bootstrap";
+import how1_en from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso01_EN.png';
+import how1_es from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso01_ES.png';
+import how1_pt from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso01_PT.png';
+import how2_es from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso02_ES.png';
+import how2_pt from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso02_PT.png';
+import how2_en from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso02_EN.png';
+import how3_es from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso03_ES.png';
+import how3_pt from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso03_PT.png';
+import how3_en from '../../asset/imgs/encuestas/EncuestasAutomatizada_Paso03_EN.png';
 
+const how1_lang = {es: how1_es, pt: how1_pt, en: how1_en};
+const how2_lang = {es: how2_es, pt: how2_pt, en: how2_en};
+const how3_lang = {es: how3_es, pt: how3_pt, en: how3_en};
 export default function Market() {
     const { t } = useTranslation(['encuestas', 'common']);
-
+    const { locale } = useRouter();
+    const how1 = how1_lang[locale] || how1_lang["es"];
+    const how2 = how2_lang[locale] || how2_lang["es"];
+    const how3 = how3_lang[locale] || how3_lang["es"];
     return (
         <>
             <Container fluid className="flow-encuestas-section relative">
