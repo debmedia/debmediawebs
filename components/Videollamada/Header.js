73ed76f36700315d174499b625735b9c08e79b17
Agustín Queija <agustin.queija@gmail.com>
2023-03-07 15:20:33 -0300

    Agregar video

--- components/Videollamada/Header.js
+++ components/Videollamada/Header.js
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Row, Container, Col } from "react-bootstrap";
+import { Row, Container, Col, Modal } from "react-bootstrap";
 import Image from 'next/image'
-import mypic from '../../asset/imgs/videollamada/main.png'
-import mypicxs from '../../asset/imgs/videollamada/main-videocall.png'
 import shape1 from '../../asset/imgs/home/headershape1.svg'
 import shape2 from '../../asset/imgs/home/headershape2.svg'
 import shape3 from '../../asset/imgs/home/headershape3.svg'
-import { useMediaQuery } from 'react-responsive'
+import videoThumb_es from '../../asset/imgs/videollamada/video_thumb_ES.png';
+import videoThumb_xs_es from '../../asset/imgs/videollamada/video_thumb_xs_ES.png';
 
 
 export default function Header() {
-    const isMobile = useMediaQuery({ query: '(max-width:899px)' })
-
+    const [modalShow, setModalShow] = React.useState(false);
     return (
         <>
             <header>
                 <Container fluid className="videocall-home">
-                    <div className={'image-container-videocall'}>
-                        <Image src={mypic.src} layout="fill" className={'image d-none d-sm-block'} />
-                        <Image src={mypicxs.src} layout="fill" className={'image d-block d-sm-none'} />
+                    <div className={'image-container-videocall'} onClick={() => setModalShow(true)}>
+                        <Image src={videoThumb_es} layout="fill" className={'image d-none d-sm-block'} />
+                        <Image src={videoThumb_xs_es} layout="fill" className={'image d-block d-sm-none'} />
                     </div>
                     <div className="d-none d-sm-block">
                         <img src={shape3.src} className="videocall-home_shape3" />
@@ -28,11 +26,15 @@ export default function Header() {
                         <img src={shape1.src} className="videocall-home_shape1" />
                         <img src={shape2.src} className="videocall-home_shape2" />
                     </div>
+                    <MyVerticallyCenteredModal
+                        show={modalShow}
+                        onHide={() => setModalShow(false)}
+                    />
                     <Container>
                         <Row>
                             <Col className="mainTitles">
                                 <h1 className="videocall-home_title">
-                                    Digitaliza la atención
+                                    Digitaliza la atención  
                                     <br className="d-none d-sm-block" />de tus clientes
                                 </h1>
                                 <p className="videocall-home_parraf">
@@ -56,5 +58,21 @@ export default function Header() {
                 </Row>
             </Container>
         </>
+    );
+}
+
+
+function MyVerticallyCenteredModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Body>
+                <iframe width="100%" height="500" src="https://www.youtube-nocookie.com/embed/Kqr77nXvcXY?autoplay=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            </Modal.Body>
+        </Modal>
     );
 }
