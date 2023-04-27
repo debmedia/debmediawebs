ec6ddf7e986d551fe4ae66cd552dbf26f3cd2c49
Agustín Queija <agustin.queija@gmail.com>
2023-04-27 12:44:36 -0300

    Agregar los componentes a la home del blog

--- pages/blog-new/index.js
+++ pages/blog-new/index.js
@@ -3,6 +3,9 @@ import BlogNavbar from '../../components/Blog/BlogNavbar';
 import wpApi from '../../config/axios'
 import { WP_POSTS_URL } from '../../constants/urls'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import CategoryNav from '../../components/Blog/CategoryNav';
+import HeroPostCard from '../../components/Blog/HeroPostCard';
+import testPosts from '../../json/testPosts_no_comitear.json';
 // export async function getStaticProps(context) {
 //     try {
 //         const res = await wpApi.get(WP_POSTS_URL);
@@ -21,17 +24,21 @@ export async function getStaticProps({ locale }) {
     return {
       props: {
         ...(await serverSideTranslations(locale, ['blogHome','components','common'])),
+        postsData: testPosts,
       },
     };
   }
 
 export default function BlogHome({postsData}) {
   return (
-    <>
+    <div className='blog'>
         <BlogNavbar></BlogNavbar>
+        <div style={{height: "110px"}}></div>
+        <CategoryNav/>
+        <HeroPostCard post={postsData[0]}/>
         <div>
             <div style={{minHeight: "900px", paddingTop: "100px"}}>BlogHome </div>
         </div>
-    </>
+    </div>
   )
 }
