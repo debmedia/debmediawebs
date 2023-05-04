a331e01fb15caa2999d7bb2e10a71af8a1580bb3
Agustín Queija <agustin.queija@gmail.com>
2023-05-04 18:14:48 -0300

    Imagenes de gracias

--- components/Thankyou.js
+++ components/Thankyou.js
@@ -1,36 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { Row, Container, Col } from "react-bootstrap";
-import Image from 'next/image'
-import woman from '../asset/imgs/gracias-woman.svg'
-import womanxs from '../asset/imgs/gracias-womanxs.svg'
-import shape2 from '../asset/imgs/contact/right.svg'
-import HubspotForm from 'react-hubspot-form'
-import Link from 'next-translate-routes/link'
-import { useTranslation, Trans } from 'next-i18next';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Image from "next/image";
+import woman_es from "../asset/imgs/gracias-woman-ES.svg";
+import woman_pt from "../asset/imgs/gracias-woman-PT.svg";
+import woman_en from "../asset/imgs/gracias-woman-EN.svg";
+import Link from "next-translate-routes/link";
+import { useTranslation, Trans } from "next-i18next";
 import { useRouter } from "next/dist/client/router";
 
-
+const woman_lang = { es: woman_es, pt: woman_pt, en: woman_en };
 export default function Thankyou() {
-    const { t } = useTranslation(['components', 'common']);
+    const { t } = useTranslation(["components", "common"]);
     const { locale } = useRouter();
+    const woman = woman_lang[locale] || woman_lang["es"];
     return (
-        <>
-            <Container fluid className="gracias-section py-5">
-                <div className={'gracias-section-woman'}>
-                    <Image src={woman.src} layout="fill" className={'d-none d-sm-block'} />
-                    <Image src={womanxs.src} layout="fill" className={'d-block d-sm-none'} />
-                </div>
-                {locale === "es" && <div className="gracias-section_blue">
+        <Container fluid className="gracias-section px-0">
+            <div className={"gracias-section-woman"}>
+                <Image src={woman} layout="fill" className={""} objectFit="fill" />
+            </div>
+            {locale === "es" && (
+                <div className="gracias-section_blue">
                     <h1>
-                        <Trans t={t} i18nKey="THANKYOU.MESSAGE"/>
+                        <Trans t={t} i18nKey="THANKYOU.MESSAGE" />
                     </h1>
                     <Link href="/">
                         <button className="citas-home_demobutton">
-                            <Trans t={t} i18nKey="THANKYOU.BUTTON"/>
+                            <Trans t={t} i18nKey="THANKYOU.BUTTON" />
                         </button>
                     </Link>
-                </div>}
-            </Container>
-        </>
+                </div>
+            )}
+        </Container>
     );
 }
