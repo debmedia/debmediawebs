e6b638713845ae932551855f747e1d926d5dd3e6
Agustín Queija <agustin.queija@gmail.com>
2023-05-15 20:43:39 -0300

    error management en utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,10 @@
 export const calcReadingTime = (text) => {
     const averageWordsPerMinute = 200;
-    const words = text.match(/\S+/g).length;
-    return Math.ceil(words / averageWordsPerMinute);
+    try {
+        const words = text.match(/\S+/g).length;
+        return Math.ceil(words / averageWordsPerMinute);
+    } catch(err){
+        console.error("[calcReadingTime]: " + err.message);
+        return 5; // devolvemos 5 para que muestre algo
+    }
 };
