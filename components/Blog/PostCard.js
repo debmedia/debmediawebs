961ada85c9cd0df3ebfa86d86866e45cef49bba5
Agustín Queija <agustin.queija@gmail.com>
2023-05-16 14:48:55 -0300

    Poner link a tag

--- components/Blog/PostCard.js
+++ components/Blog/PostCard.js
@@ -9,52 +9,54 @@ export default function PostCard({ post }) {
     const compact = false;
     return (
         <div className={`post-card ${compact && "compact"}`}>
-            <Link href={post.link}>
-                <div className="card-wrapper">
-                        {/* TODO: ver si se puede optimizar la imagen */}
-                        <div className="imageContainer">
-                            <Image
-                                src={post.featuredImage.node.mediaItemUrl}
-                                layout="fill"
-                                objectFit="cover"
-                                alt={post.title}
-                                ></Image>
-                        </div>
-                    <div className="card-content-container">
-                        <div className="card-content">
-                            {post.categories.edges.find((elem) => elem.isPrimary) ?
-                            <Badge className="mb-2" bg="primary">{post.categories.edges.find((elem) => elem.isPrimary)?.node.name}</Badge>
-                            :
-                            // badge transparente vacio para mantener el espacio
-                            <Badge className="mb-2 hidden" bg="primary">&nbsp;</Badge>
-                            }
-                            <h3
-                                className="title"
-                                dangerouslySetInnerHTML={{
-                                    __html: post.title,
-                                }}></h3>
-                            {!compact && (
-                                <>
-                                    <p
-                                        className="excerpt my-1"
-                                        dangerouslySetInnerHTML={{
-                                            __html: post.excerpt,
-                                        }}></p>
-                                    <div className="debLink">
-                                        Leer más <i className="bi bi-caret-right"></i>
-                                    </div>
-                                </>
-                            )}
-                        </div>
-                        <div className="card-footer">
-                            <hr></hr>
-                            <AuthorCard
-                                author={post.author.node}
-                                createdDate={post.dateGmt}
-                                readTime={calcReadingTime(post.content)}></AuthorCard>
+            <Link href={post.link} passHref>
+                <a className="reset-a">
+                    <div className="card-wrapper">
+                            {/* TODO: ver si se puede optimizar la imagen */}
+                            <div className="imageContainer">
+                                <Image
+                                    src={post.featuredImage.node.mediaItemUrl}
+                                    layout="fill"
+                                    objectFit="cover"
+                                    alt={post.title}
+                                    ></Image>
+                            </div>
+                        <div className="card-content-container">
+                            <div className="card-content">
+                                {post.categories.edges.find((elem) => elem.isPrimary) ?
+                                <Badge className="mb-2" bg="primary">{post.categories.edges.find((elem) => elem.isPrimary)?.node.name}</Badge>
+                                :
+                                // badge transparente vacio para mantener el espacio
+                                <Badge className="mb-2 hidden" bg="primary">&nbsp;</Badge>
+                                }
+                                <h3
+                                    className="title"
+                                    dangerouslySetInnerHTML={{
+                                        __html: post.title,
+                                    }}></h3>
+                                {!compact && (
+                                    <>
+                                        <p
+                                            className="excerpt my-1"
+                                            dangerouslySetInnerHTML={{
+                                                __html: post.excerpt,
+                                            }}></p>
+                                        <div className="debLink">
+                                            Leer más <i className="bi bi-caret-right"></i>
+                                        </div>
+                                    </>
+                                )}
+                            </div>
+                            <div className="card-footer">
+                                <hr></hr>
+                                <AuthorCard
+                                    author={post.author.node}
+                                    createdDate={post.dateGmt}
+                                    readTime={calcReadingTime(post.content)}></AuthorCard>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </Link>
         </div>
     );
