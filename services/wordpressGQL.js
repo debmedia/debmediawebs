63197b7871afa53b68cc365a8c62d57e447a840e
Agustín Queija <agustin.queija@gmail.com>
2023-06-15 09:03:06 -0300

    servicios para buscar los post por cateogoiras

--- services/wordpressGQL.js
+++ services/wordpressGQL.js
@@ -74,12 +74,50 @@ export const QUERY_GET_POSTS = gql`
     }
 `;
 
-                    }
-                }
+export const QUERY_GET_POSTS_BY_CATEGORY_ID = gql`
+    ${CORE_POST_FIELDS}
+    query getPosts($first: Int, $after: String, $categoryId: Int) {
+        posts(first: $first, after: $after, where: { status: PUBLISH, categoryId: $categoryId}) {
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            nodes {
+                ...CorePostFields
             }
         }
     }
 `;
+
+export const QUERY_GET_ROOT_CATEGORIES = gql`
+    query getCategories {
+        categories (where: {parent: null}) {
+            nodes  {
+                slug
+                name 
+                uri
+                databaseId
+                children {
+                    nodes {
+                        slug
+                    }
+                } 
+            }
+        }
+    }
+`
+
+export const QUERY_GET_CATEGORIES_BY_SLUG = gql`
+    query getCategoriesBySlug($slug: String) {
+        categories(where: {slug: [$slug]}) {
+            nodes {
+            databaseId
+            slug
+            name
+            }
+        }
+    }
+`
 
 export async function getPostBySlug(slug) {
     const res = await apolloClient.query({
@@ -94,6 +132,24 @@ export async function getPostBySlug(slug) {
 export async function getPosts({first, after}) {
     const res = await apolloClient.query({variables:{first, after}, query: QUERY_GET_POSTS});
     console.log(res);
+    return {
+        posts: res.data.posts.nodes,
+        pagination: res.data.posts.pageInfo
+    }
+}
+
+export async function getRootCategories() {
+    const res = await apolloClient.query({query: QUERY_GET_ROOT_CATEGORIES});
+    return res.data.categories.nodes;
+}
+
+export async function getCategoriesBySlug(slug) {
+    const res = await apolloClient.query({variables:{slug}, query: QUERY_GET_CATEGORIES_BY_SLUG});
+    return res.data.categories.nodes;
+}
+
+export async function getPostByCategoryId({first, after, categoryId}) {
+    const res = await apolloClient.query({variables:{first, after, categoryId}, query: QUERY_GET_POSTS_BY_CATEGORY_ID});
     return {
         posts: res.data.posts.nodes,
         pagination: res.data.posts.pageInfo
