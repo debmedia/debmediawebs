1e25209b6d8c64ac97645fb24b11a769d922b27c
Agustín Queija <agustin.queija@gmail.com>
2023-06-21 10:36:29 -0300

    agregar paths a renderear

--- pages/blog-new/categorias/[categorySlug].js
+++ pages/blog-new/categorias/[categorySlug].js
@@ -14,15 +14,75 @@ import RelatedPostsSection from '../../../components/Blog/RelatedPostsSection';
 import { ObjectInspector, chromeDark } from 'react-inspector';
 import { QUERY_GET_POSTS_BY_CATEGORY_ID } from "../../../services/wordpressGQL";
 
+// TODO: buscar las categorias directamente de wp
+const categories = [
+    {
+        "slug": "banca-y-seguros",
+        "name": "Banca y seguros",
+        "databaseId": 1354
+    },
+    {
+        "slug": "casos-de-exito",
+        "name": "Casos de éxito",
+        "databaseId": 855
+    },
+    {
+        "slug": "ebooks",
+        "name": "Ebooks",
+        "databaseId": 856
+    },
+    {
+        "slug": "gobierno",
+        "name": "Gobierno",
+        "databaseId": 1355
+    },
+    {
+        "slug": "industrias",
+        "name": "Industrias",
+        "databaseId": 1353
+    },
+    {
+        "slug": "noticias",
+        "name": "Notas",
+        "databaseId": 3
+    },
+    {
+        "slug": "novedades",
+        "name": "Novedades",
+        "databaseId": 2
+    },
+    {
+        "slug": "podcast",
+        "name": "Podcast",
+        "databaseId": 1382
+    },
+    {
+        "slug": "retail",
+        "name": "Retail",
+        "databaseId": 1357
+    },
+    {
+        "slug": "salud",
+        "name": "Salud",
+        "databaseId": 1358
+    },
+    {
+        "slug": "telecomunicaciones",
+        "name": "Telecomunicaciones",
+        "databaseId": 1356
+    },
+    {
+        "slug": "utilities",
+        "name": "Utilities",
+        "databaseId": 1359
+    }
+];
+
 export async function getStaticPaths() {
 
-    //TODO: configurar la paginas que se generan en build time
-    // const posts = await getPosts({first: 1});
     return {
-      paths: [
-        // { params: { postSlug: posts[0].slug } },
-      ],
-      fallback: 'blocking',
+      paths: categories.map((category)=> {return {params: {categorySlug: category.slug}}}),
+      fallback: false,
     }
   }
 
