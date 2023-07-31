5254e1cbdf569bdd14a6747133c436cbc979578f
Agustín Queija <agustin.queija@gmail.com>
2023-07-31 15:59:46 -0300

    tradu Podcast section

--- components/Blog/BlogHome/PodcastSection.js
+++ components/Blog/BlogHome/PodcastSection.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import PodcastCard from './PodcastCard'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import PodcastCard from './PodcastCard';
+import { Container } from 'react-bootstrap';
+import { useTranslation, Trans } from 'next-i18next';
 
 export default function PodcastSection({post}) {
+  const { t } = useTranslation(['blogHome', 'common']);
   return (
     <Container className='podcast-section my-5'>
-        <h2 className='text-center mb-3 fw-bold'>Podcast</h2>
+        <h2 className='text-center mb-3 fw-bold'>{t("PODCAST_SECTION.TITLE")}</h2>
         <PodcastCard post={post}></PodcastCard>
     </Container>
   )
