5e04452f2b56783dc15adb8e7b5fe786d8de298b
Agustín Queija <agustin.queija@gmail.com>
2024-05-07 09:29:54 -0300

    agregar videos

--- components/NewNameVideoPopup.js
+++ components/NewNameVideoPopup.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Modal } from 'react-bootstrap';
-import videoPosterImage_en from '../asset/imgs/newNameVideoPoster_es.svg';
-import videoPosterImage_es from '../asset/imgs/newNameVideoPoster_en.svg';
-import videoPosterImage_pt from '../asset/imgs/newNameVideoPoster_pt.svg';
+// import videoPosterImage_en from '../asset/imgs/newNameVideoPoster_es.svg';
+// import videoPosterImage_es from '../asset/imgs/newNameVideoPoster_en.svg';
+// import videoPosterImage_pt from '../asset/imgs/newNameVideoPoster_pt.svg';
 import { useRouter } from 'next-translate-routes';
 
 const DELAY_TO_CLOSE_MODAL_AFTER_VIDEO_END_MILLISECONDS = 1000;
@@ -11,12 +11,12 @@ function NewNameVideoPopup() {
     const [show, setShow] = React.useState(true);
     const {locale} = useRouter();
 
-    let videoPoster = videoPosterImage_es.src;
-    if (locale === "en") {
-        videoPoster = videoPosterImage_en.src;
-    } else if (locale === "pt") {
-        videoPoster = videoPosterImage_pt.src;
-    }
+    // let videoPoster = videoPosterImage_es.src;
+    // if (locale === "en") {
+    //     videoPoster = videoPosterImage_en.src;
+    // } else if (locale === "pt") {
+    //     videoPoster = videoPosterImage_pt.src;
+    // }
 
     const handleVideoEnd = () => {
         setTimeout(() => {
@@ -33,7 +33,7 @@ function NewNameVideoPopup() {
     return (
         <Modal show={show} fullscreen={true} onHide={handleOnClose} dialogClassName="modal-falopa">
             <div style={{background: "black", width:"100%", height:"100%"}}>
-                <video width="100%" height="100%" muted autoPlay src={`/videos/newNameVideo_${locale}.mp4`} onEnded={handleVideoEnd} disablepictureinpicture poster={videoPoster}/>
+                <video width="100%" height="100%" muted autoPlay src={`/videos/newNameVideo_${locale}.mp4`} onEnded={handleVideoEnd} disablepictureinpicture/>
             </div>
         </Modal>    
     );
